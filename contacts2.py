994cf2829ae0f261816fa52031c74df11473eae9
pvalle <paul.valle@orbian.com>
2019-03-01 13:51:36 +0000

    looking at rollback

--- contacts2.py
+++ contacts2.py
@@ -2,11 +2,15 @@ import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
 
-update_sql = "UPDATE contacts SET email = 'update@example.com' WHERE contacts.phone = 123456"
+new_email = "update2@example.com"
+phone = input("Please enter phone number")
+
+# update_sql = "UPDATE contacts SET email = '{}' WHERE contacts.phone = {}".format(new_email, phone)
+update_sql = "UPDATE contacts SET email = ? WHERE contacts.phone = ?"
 
 # cursors do not commit
 update_cursor = db.cursor()
-update_cursor.execute(update_sql)
+update_cursor.execute(update_sql, (new_email, phone))  # for single value notice comma (<value1>,)
 print("{} rows updated".format(update_cursor.rowcount))
 
 # use the below to commit the change
