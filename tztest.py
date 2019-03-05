c2044c1ad6dcec6871570d563c162b9900acee56
pvalle <paul.valle@orbian.com>
2019-03-05 13:44:36 +0000

    looking at a music database using the knowledge learned

--- tztest.py
+++ tztest.py
@@ -1,11 +1,13 @@
 import sqlite3
 import datetime
 import pytz
+import pickle
 
 db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 db.execute("CREATE TABLE IF NOT EXISTS accounts (name TEXT PRIMARY KEY NOT NULL, balance INTEGER NOT NULL)")
 db.execute("CREATE TABLE IF NOT EXISTS history (time TIMESTAMP NOT NULL,"
-           " account TEXT NOT NULL, amount INTEGER NOT NULL, PRIMARY KEY (amount, time, account))")
+           " account TEXT NOT NULL, amount INTEGER NOT NULL, zone INTEGER NOT NULL,"
+           " PRIMARY KEY (amount, time, account))")
 db.execute("CREATE VIEW IF NOT EXISTS localhistory AS"
            " SELECT strftime('%Y-%m-%d %H:%M:%f', history.time, 'localtime') AS localtime,"
            " history.account, history.amount FROM history ORDER BY history.time")
@@ -15,9 +17,13 @@ class Account(object):
 
     @staticmethod
     def _current_time():
-        return pytz.utc.localize(datetime.datetime.utcnow())
+        # return pytz.utc.localize(datetime.datetime.utcnow())
         # local_time = pytz.utc.localize(datetime.datetime.utcnow())
         # return local_time.astimezone()
+        utc_time = pytz.utc.localize(datetime.datetime.utcnow())
+        local_time = utc_time.astimezone()
+        zone = local_time.tzinfo
+        return utc_time, zone       # return a tuple
 
     def __init__(self, name: str, opening_balance: int = 0):
         cursor = db.execute("SELECT name, balance FROM accounts WHERE (name = ?)", (name,))
@@ -36,15 +42,13 @@ class Account(object):
 
     def _save_update(self, amount):
         new_balance = self._balance + amount
-        deposit_time = Account._current_time()
-        try:
-            db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", (new_balance, self.name))
-            db.execute("INSERT INTO history VALUES(?, ?, ?)", (deposit_time, self.name, amount))
-        except sqlite3.Error:
-            db.rollback()
-        else:
-            db.commit()
-            self._balance = new_balance
+        deposit_time, zone = Account._current_time()  # unpacking return tuple
+        pickle_zone = pickle.dumps(zone)
+        db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", (new_balance, self.name))
+        print("{}\t{}".format(amount, deposit_time))
+        db.execute("INSERT INTO history VALUES(?, ?, ?, ?)", (deposit_time, self.name, amount, pickle_zone))
+        db.commit()
+        self._balance = new_balance
 
     def deposit(self, amount: int) -> float:
         if amount > 0.0:
