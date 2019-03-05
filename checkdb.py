cc9ea52764e35c1ccb0dce137d33f506bf25fe7f
pvalle <paul.valle@orbian.com>
2019-03-05 08:54:43 +0000

    looking at rollback

--- checkdb.py
+++ checkdb.py
@@ -12,7 +12,6 @@ db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 # for row in db.execute("SELECT strftime('%Y-%m-%d %H:%M:%f', history.time, 'localtime') AS localtime,"
 #                        " history.account, history.amount FROM history ORDER BY history.time"):
 for row in db.execute("SELECT * from localhistory"):
-     print(row)
-
+    print(row)
 
 db.close()
